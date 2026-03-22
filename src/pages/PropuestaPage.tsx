e7239fb5ee370d68f90467e56f84af2cc0b833b9
GGLPRO <guillermogonzalezleon@gmail.com>
2026-03-22 09:31:02 -0300

    feat(agent): src/pages/PropuestaPage.tsx via hoku

---
+++ src/pages/PropuestaPage.tsx
@@ -0,0 +1,13 @@
+// Página de la Propuesta de Consultoría SAP
+
+import ResumenPropuesta from "../components/ResumenPropuesta";
+
+const PropuestaPage = () => {
+  return (
+    <div>
+      <ResumenPropuesta />
+    </div>
+  );
+};
+
+export default PropuestaPage;
