198ab8bab9d43ddab9d9c3a0ad43139bcabf2343
GGLPRO <guillermogonzalezleon@gmail.com>
2026-03-23 16:57:42 -0300

    feat(agent): src/App.tsx via hoku

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SmartConnection from './components/SmartConnection';
+
+const App = () => {
+  return (
+    <div>
+      <SmartConnection dispositivo="Dispositivo 1" red="Red 1" />
+    </div>
+  );
+};
